279dc554bf10066158fcdc5e4a0af48306a180f5
Игорь <mc_grove@bk.ru>
2024-01-31 17:45:25 +0300

    hw-2 update

--- homeworkshops/homeworkshops/settings.py
+++ homeworkshops/homeworkshops/settings.py
@@ -58,7 +58,7 @@ LOGGING = {
             'handlers': ['console', 'file'],
             'level': 'INFO',
         },
-        'hwapp': {
+        'onlineshop': {
             'handlers': ['console', 'file'],
             'level': 'DEBUG',
             'propagate': True,
